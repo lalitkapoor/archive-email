f8fc59aa07f76b35a1b2e6feb18fe775125264d6
Lalit Kapoor <lalitkapoor@gmail.com>
2012-10-19 20:28:02 -0500

    limiting data coming back to just [from, to, subject, message_id, date, body]

--- routes/v1/messages.js
+++ routes/v1/messages.js
@@ -9,10 +9,25 @@ var
 
 messages.get = function (req, res){
   context.accounts(req.params.id).messages().get(
-    {folder: req.params.folder}
+    {
+      folder: req.params.folder
+    , include_body: 1
+    }
   , function(err, response){
+      var results = [];
+      for(var i=0; i<response.body.length; i++){
+        var message = response.body[i];
+        results.push({
+          from: message.addresses.from
+        , to: message.addresses.to
+        , subject: message.subject
+        , message_id: message.message_id
+        , date: new Date(message.date*1000)
+        , body: message.body[0].content
+        })
+      }
       //console.log(util.inspect(response.body, false, null, true));
-      res.json(response.body);
+      res.json(results);
     }
   );
 };
