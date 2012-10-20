f44ad27ecd6c9cd45fad190b8e0e7bbd7e38f4bc
Lalit Kapoor <lalitkapoor@gmail.com>
2012-10-20 00:51:33 -0500

    made tag/folder a parameter. added the ability to get only a single message

--- router.js
+++ router.js
@@ -5,6 +5,7 @@ var
 ;
 
 router.init = function(app){
+  // Enable CORS
   app.all('*', function(req, res, next){
     res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
     res.header('Access-Control-Allow-Origin', res.req.headers['origin']);
@@ -15,7 +16,8 @@ router.init = function(app){
     next();
   });
   //Messages
-  app.get('/v1/:id/messages/:tag', routes.v1.messages.get);
+  app.get('/v1/:id/messages', routes.v1.messages.list);
+  app.get('/v1/:id/messages/:messageId', routes.v1.messages.get);
 };
 
 module.exports = router;
--- routes/v1/messages.js
+++ routes/v1/messages.js
@@ -8,10 +8,10 @@ var
 , messages =  {}
 ;
 
-messages.get = function (req, res){
+messages.list = function (req, res){
   context.accounts(req.params.id).messages().get(
     {
-      folder: req.params.folder
+      folder: req.query.folder
     , include_body: 1
     , include_flags: 1
     }
@@ -20,18 +20,32 @@ messages.get = function (req, res){
       for(var i=0; i<response.body.length; i++){
         var message = response.body[i];
         results.push({
-          from: message.addresses.from
-        , to: message.addresses.to
+          to: message.addresses.to
+        , from: message.addresses.from
+        , fromDomain: message.addresses.from.email.match(/@(\w*)\./)[2]
         , subject: message.subject
-        , message_id: message.message_id
+        , messageId: message.message_id
         , date: new Date(message.date*1000)
         , body: sanitize(message.body[0].content.replace(/(\r\n|\n|\r)/gm,"").replace(/(\t)/gm, " ")).trim().substr(0, 140)
         , read: (message.flags && message.flags[0] != null && message.flags[0] === '\\Seen') ? true : false
         , labels: message.folders
+        , tumbnails: messsage.personal_info
         });
       }
       //console.log(util.inspect(response.body, false, null, true));
-      res.json(results);
+      res.json(response.body);
+    }
+  );
+};
+
+messages.get = function(req, res) {
+  context.accounts(req.params.id).messages(req.params.messageId).body().get(
+    {
+      type: 'text/html'
+    }
+  , function(err, response){
+      //console.log(util.inspect(response.body, false, null, true));
+      res.send(response.body[0].content);
     }
   );
 };
